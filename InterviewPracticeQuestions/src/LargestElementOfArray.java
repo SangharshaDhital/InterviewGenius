93b93c199fd3c1bf6548bd35f5e84a3396e15f1b
Sangharsha <sdhital4@mail.ccsf.edu>
2023-05-21 22:19:04 -0700

    First Commit

--- InterviewPracticeQuestions/src/LargestElementOfArray.java
+++ InterviewPracticeQuestions/src/LargestElementOfArray.java
@@ -5,44 +5,42 @@ public class LargestElementOfArray {
 
 	public static void main(String[] args) {
 		System.out.println("Java program to find the largest element in an array.");
-		int[] arr= arrayInput();
-		 int largest =largestElementArray(arr);
-		 System.out.println("\nThe largest Eement of Given Array is: "+largest);
+		int[] arr = arrayInput();
+		int largest = largestElementArray(arr);
+		System.out.println("\nThe largest Eement of Given Array is: " + largest);
 	}
-		
-	
-		public static int[] arrayInput(){
-		Scanner input=new Scanner(System.in);
+
+	public static int[] arrayInput() {
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the no. of Elements in an Array:");
-		int n=input.nextInt();
-		int[] arr=new int[n];
-		System.out.println("Great! Enter "+n+" Integers of an Array:");
-		
-		for(int i=0;i<n;i++) {
-			arr[i]=input.nextInt();
+		int n = input.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Great! Enter " + n + " Integers of an Array:");
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = input.nextInt();
+		}
+		System.out.print("Your Array is : '[");
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i]);
+			if (i != n - 1) {
+				System.out.print(", ");
 			}
-			System.out.print("Your Array is : '[");
-			for(int i=0;i<n;i++) {
-					System.out.print(arr[i]);
-				if(i!=n-1){
-					System.out.print(", ");
-				}
-		
-	}
-			System.out.print("]'");
-			return arr;
+
+		}
+		System.out.print("]'");
+		return arr;
 
 	}
 
-		public static int largestElementArray(int arr[]){
-			int largest=arr[0];
-			for(int i=0;i<arr.length;i++) {
-				if(arr[i]>largest) {
-					largest=arr[i];
-				}
-			}		
-			return largest;
-			
-}
-}
+	public static int largestElementArray(int arr[]) {
+		int largest = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > largest) {
+				largest = arr[i];
+			}
+		}
+		return largest;
 
+	}
+}
